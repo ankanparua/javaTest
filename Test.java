725ee620fce75631110325ecdad889aed6e99df2
ankanparua <rickyparua44@gmail.com>
2026-01-29 15:30:15 +0530

    Update4 Test.java

--- Test.java
+++ Test.java
@@ -31,7 +31,7 @@ public class Test {
     // Main method
     public static void main(String[] args) {
         int x = 20;
-        int y = 2;
+        int y = 4;
 
         add(x, y);
         sub(x, y);
